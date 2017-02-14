071876934e3ba7d7bed5a39d15a05458f47ea560
Robson Marques <robsonmarques@gmail.com>
2017-02-14 06:01:33 -0200

    Inclui documentação para Sessions
    Adiciona a documentação para os endpoints de login e logout.

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,8 +1,53 @@
 defmodule Desqer.SessionController do
+  @moduledoc """
+  Endpoints for sessions.
+  """
   use Desqer.Web, :controller
 
   plug :scrub_params, "user" when action in [:create]
 
+  @doc """
+  Signs in `user`.
+
+  Endpoint example:
+
+      POST /sessions
+
+  Params example:
+
+      {
+        "user": {
+          "phone": "+55 47 99987-4321",
+          "password": "foo123"
+        }
+      }
+
+  Success response `code 200` example:
+
+      {
+        "jwt": "eyJhbGciOiJIUzUxMiI5c...",
+        "exp": 1489573943,
+        "data": {
+          "id": "2050ea22-a273-4bef-93e9-ce9df0e73ddc",
+          "name": "John Doe",
+          "phone": "55479995874321",
+          "email": "john@doe.com",
+          "bio": "An awesome professional"
+          "professional": true,
+          "confirmed": true,
+          "deleted": false,
+        }
+      }
+
+  Error response `code 422` example:
+
+      {
+        "errors": {
+          "phone": ["can't be blank"],
+          "password": ["can't be blank"]
+        }
+      }
+  """
   def create(conn, %{"user" => user_params}) do
     case Desqer.Action.CreateSession.run(user_params, user_agent(conn), conn.remote_ip) do
       {:ok, user, jwt, exp} ->
@@ -17,6 +62,32 @@ defmodule Desqer.SessionController do
     end
   end
 
+  @doc """
+  Signs out `user`.
+
+  Endpoint example:
+
+      DELETE /sessions
+
+  Request headers example:
+
+      authorization: Bearer eyJhbGciOiJIUzUxMiI5c...
+
+
+  Success response `code 200` example:
+
+      {
+        "detail": "Logged out"
+      }
+
+  Error response `code 401` example:
+
+      {
+        "errors": {
+          "detail": "Resource not authorized"
+        }
+      }
+  """
   def delete(conn, _params) do
     conn
     |> Guardian.Plug.current_resource
