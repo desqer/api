08a652113eb24105f39055895268515797d35dac
Robson Marques <robsonmarques@gmail.com>
2017-03-09 05:22:39 -0300

    Ajusta a documentação de appointments
    Não é possível criar duas documentações diferentes para duas
    funções com o mesmo nome e número de argumentos. Dessa forma,
    inclui a documentação para os dois casos na mesma função.

--- web/controllers/appointment_controller.ex
+++ web/controllers/appointment_controller.ex
@@ -8,7 +8,7 @@ defmodule Desqer.AppointmentController do
   plug :scrub_params, "appointment" when action in [:create, :update]
 
   @doc """
-  Creates `appointments` by professional.
+  Creates `appointment` or `appointments`.
 
   Endpoint example:
 
@@ -18,7 +18,18 @@ defmodule Desqer.AppointmentController do
 
       authorization: Bearer eyJhbGciOiJIUzUxMiI5c...
 
-  Params example:
+  Params example by customer:
+
+      {
+        "appointment": {
+          "service_id": "80e9863b-61bc-45be-9f0e-9aebfb2dcec6",
+          "starts_at": "2017-03-10 15:30:00",
+          "ends_at": "2017-03-10 16:00:00",
+          "notes": "Hair massage needed"
+        }
+      }
+
+  Params example by professional:
 
       {
         "appointment": {
@@ -39,7 +50,26 @@ defmodule Desqer.AppointmentController do
 
   - `price` in cents
 
-  Success response `code 200` example:
+  Success response `code 200` example for customer:
+
+      {
+        "data": {
+          "appointment": {
+            "id": "2050ea22-a273-4bef-93e9-ce9df0e73ddc",
+            "user_id": "69f06156-7fc2-4c7f-abde-fc20204627e1",
+            "service_id": "9c1c0135-9cb7-41d6-9991-9c8042d31bc8",
+            "name": "Haircut",
+            "description": "The perfect hair style for your type of face",
+            "price": 4900,
+            "starts_at": "2017-03-10T15:30:00",
+            "ends_at": "2017-03-10T16:00:00",
+            "notes": "Hair massage needed",
+            "status": "active"
+          }
+        }
+      }
+
+  Success response `code 200` example for professional:
 
       {
         "data": {
@@ -84,68 +114,6 @@ defmodule Desqer.AppointmentController do
     end
   end
 
-  @doc """
-  Creates `appointment`.
-
-  Endpoint example:
-
-      POST /appointments
-
-  Request headers example:
-
-      authorization: Bearer eyJhbGciOiJIUzUxMiI5c...
-
-  Params example:
-
-      {
-        "appointment": {
-          "service_id": "80e9863b-61bc-45be-9f0e-9aebfb2dcec6",
-          "starts_at": "2017-03-10 15:30:00",
-          "ends_at": "2017-03-10 16:00:00",
-          "notes": "Hair massage needed"
-        }
-      }
-
-  Params details:
-
-  - `price` in cents
-
-  Success response `code 200` example:
-
-      {
-        "data": {
-          "appointment": {
-            "id": "2050ea22-a273-4bef-93e9-ce9df0e73ddc",
-            "user_id": "69f06156-7fc2-4c7f-abde-fc20204627e1",
-            "service_id": "9c1c0135-9cb7-41d6-9991-9c8042d31bc8",
-            "name": "Haircut",
-            "description": "The perfect hair style for your type of face",
-            "price": 4900,
-            "starts_at": "2017-03-10T15:30:00",
-            "ends_at": "2017-03-10T16:00:00",
-            "notes": "Hair massage needed",
-            "status": "active"
-          }
-        }
-      }
-
-  Error response `code 422` example:
-
-      {
-        "errors": {
-          "starts_at": ["can't be blank"],
-          "ends_at": ["can't be blank"]
-        }
-      }
-
-  Error response `code 401` example:
-
-      {
-        "errors": {
-          "detail": "Resource not authorized"
-        }
-      }
-  """
   def create(conn, %{"appointment" => appointment_params}) do
     case Desqer.Action.CreateAppointment.run(current_user(conn), appointment_params) do
       {:ok, appointment} -> render(conn, "show.json", appointment: appointment)
